240a4b23e0791283fbae34ced137ce644ac140b5
Colin Schmidt <colins@eecs.berkeley.edu>
2016-02-26 22:00:31 -0800

    remove malloc declaration from dhrystone
    it was unused and causing issues when building
    with the linux toolchain

--- benchmarks/dhrystone/dhrystone_main.c
+++ benchmarks/dhrystone/dhrystone_main.c
@@ -60,7 +60,6 @@ char            Ch_1_Glob,
 int             Arr_1_Glob [50];
 int             Arr_2_Glob [50] [50];
 
-extern char     *malloc ();
 Enumeration     Func_1 ();
   /* forward declaration necessary since Enumeration may not simply be int */
 
