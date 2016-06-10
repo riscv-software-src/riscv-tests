4944be4d45cafabce0519f223124d2934b9dcac5
Andrew Waterman <waterman@cs.berkeley.edu>
2016-06-09 18:47:43 -0700

    Update encoding.h

--- encoding.h
+++ encoding.h
@@ -101,14 +101,21 @@
 #define DRAM_BASE          0x80000000
 
 // breakpoint control fields
-#define BPCONTROL_X         0x00000001
-#define BPCONTROL_W         0x00000002
-#define BPCONTROL_R         0x00000004
-#define BPCONTROL_U         0x00000008
-#define BPCONTROL_S         0x00000010
-#define BPCONTROL_H         0x00000020
-#define BPCONTROL_M         0x00000040
-#define BPCONTROL_MATCHCOND 0x00000180
+#define BPCONTROL_X           0x00000001
+#define BPCONTROL_W           0x00000002
+#define BPCONTROL_R           0x00000004
+#define BPCONTROL_U           0x00000008
+#define BPCONTROL_S           0x00000010
+#define BPCONTROL_H           0x00000020
+#define BPCONTROL_M           0x00000040
+#define BPCONTROL_BPMATCH     0x00000780
+#ifdef __riscv64
+# define BPCONTROL_BPAMASKMAX 0x0F80000000000000
+# define BPCONTROL_TDRTYPE    0xF000000000000000
+#else
+# define BPCONTROL_BPAMASKMAX 0x0F800000
+# define BPCONTROL_TDRTYPE    0xF0000000
+#endif
 
 // page table entry (PTE) fields
 #define PTE_V     0x001 // Valid
