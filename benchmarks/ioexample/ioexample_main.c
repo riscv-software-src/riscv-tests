d82a880f81cb5d197f712bbd17e821e0d70d7b75
Palmer Dabbelt <palmer@dabbelt.com>
2016-07-12 08:58:48 -0700

    Start using libamf
    I split out my AMF parsing code into a library other people can use.
    It's now submoduled in.

--- benchmarks/ioexample/ioexample_main.c
+++ benchmarks/ioexample/ioexample_main.c
@@ -1,6 +1,7 @@
 // See LICENSE for license details.
 
 #include "util.h"
+#include "libamf/src/amf.h"
 
 const char *find_config_string(void)
 {
@@ -8,67 +9,6 @@ const char *find_config_string(void)
   const int config_string_addr_offset = 3;
   const char *config_string = (char *)((long)boot_rom[config_string_addr_offset]);
   return config_string;
-}
-
-int isspace(int c)
-{
-  if (c == ' ')  return 1;
-  if (c == '\t') return 1;
-  if (c == '\n') return 1;
-  if (c == '\r') return 1;
-  return 0;
-}
-
-const char *advance_until_slash(const char *path)
-{
-  while ((*path != '\0') && (*path != '/'))
-    path++;
-  if (*path == '/')
-    path++;
-
-  return path;
-}
-
-const char *advance_until_entered(const char *config_string)
-{
-  while ((*config_string != '\0') && (*config_string != '{'))
-    config_string++;
-
-  if (*config_string == '{')
-    config_string++;
-
-  return config_string;
-}
-
-const char *advance_until_over(const char *config_string)
-{
-  config_string = advance_until_entered(config_string);
-  int open = 1;
-  while ((*config_string != '\0') && (open > 0)) {
-    if (*config_string == '{') open++;
-    if (*config_string == '}') open--;
-    config_string++;
-  }
-  if (*config_string == ';')
-    config_string++;
-
-  return config_string;
-}
-
-int compare_until_slash(const char *config_string, const char *path)
-{
-  while (isspace(*config_string)) config_string++;
-  while ((*path != '\0') && (*path != '/') && (*config_string != '\0')) {
-    if (*path != *config_string)
-      return 0;
-    path++;
-    config_string++;
-  }
-
-  if (!isspace(*config_string))
-    return 0;
-
-  return 1;
 }
 
 long fromhex(char c)
@@ -91,29 +31,9 @@ long tolong(const char *config_snippet)
   return out;
 }
 
-const char *find_in_config_string(const char *config_string, const char *path)
-{
-  while (*path != '\0' && *config_string != '\0') {
-    if (compare_until_slash(config_string, path)) {
-      path = advance_until_slash(path);
-      if (*path != '\0')
-        config_string = advance_until_entered(config_string);
-    } else {
-      config_string = advance_until_over(config_string);
-    }
-  }
-
-  /* Remove any whitespace from the config string, then strip off the key. */
-  while (*config_string != '\0' &&  isspace(*config_string)) config_string++;
-  while (*config_string != '\0' && !isspace(*config_string)) config_string++;
-  while (*config_string != '\0' &&  isspace(*config_string)) config_string++;
-
-  return config_string;
-}
-
 int main( int argc, char* argv[] )
 {
-  volatile long *base = (long *)(tolong(find_in_config_string(find_config_string(), "smiexample/addr")));
+  volatile long *base = (long *)(tolong(amf_lookup(find_config_string(), "smiexample/addr")));
   printf("smiexample/addr: %lx\n", base);
 
   base[1] = 10; // Period
