5acbf6414ddaa6552ead5099868897a161bd945f
Tim Newsome <tim@sifive.com>
2017-09-01 12:03:40 -0700

    Use 32-bit link script for 32-bit target.

--- debug/targets/RISC-V/spike32.py
+++ debug/targets/RISC-V/spike32.py
@@ -7,7 +7,7 @@ class spike32_hart(targets.Hart):
     ram_size = 0x10000000
     instruction_hardware_breakpoint_count = 4
     reset_vector = 0x1000
-    link_script_path = "spike64.lds"
+    link_script_path = "spike32.lds"
 
 class spike32(targets.Target):
     harts = [spike32_hart()]
