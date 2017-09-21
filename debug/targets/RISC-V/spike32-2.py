35c41f1391b51d4d9c4e0ab40fdfc45dbea346b2
Tim Newsome <tim@sifive.com>
2017-09-21 15:19:47 -0700

    Add coverage for single-core non-rtos OpenOCD.

--- debug/targets/RISC-V/spike32-2.py
+++ debug/targets/RISC-V/spike32-2.py
@@ -5,7 +5,7 @@ import spike32  # pylint: disable=import-error
 
 class spike32_2(targets.Target):
     harts = [spike32.spike32_hart(), spike32.spike32_hart()]
-    openocd_config_path = "spike.cfg"
+    openocd_config_path = "spike-rtos.cfg"
     timeout_sec = 30
 
     def create(self):
--- debug/targets/RISC-V/spike64-2.py
+++ debug/targets/RISC-V/spike64-2.py
@@ -5,7 +5,7 @@ import spike64  # pylint: disable=import-error
 
 class spike64_2(targets.Target):
     harts = [spike64.spike64_hart(), spike64.spike64_hart()]
-    openocd_config_path = "spike.cfg"
+    openocd_config_path = "spike-rtos.cfg"
     timeout_sec = 30
 
     def create(self):
