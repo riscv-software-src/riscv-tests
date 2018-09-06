8d6a8bac53b6aeedc3e460a4b9cdcb070591f697
Michael McKeown <mmckeown@princeton.edu>
2018-09-06 16:33:12 -0700

    Enable EXTRA_INIT macro to work in VM environment (#10)

--- v/riscv_test.h
+++ v/riscv_test.h
@@ -15,6 +15,10 @@
 #undef RVTEST_CODE_BEGIN
 #define RVTEST_CODE_BEGIN                                               \
         .text;                                                          \
+        .global extra_boot;                                             \
+extra_boot:                                                             \
+        EXTRA_INIT                                                      \
+        ret;                                                            \
         .global userstart;                                              \
 userstart:                                                              \
         init
