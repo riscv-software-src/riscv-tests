73391e6d0331c8f1be206c055a00a251b7c1d4de
Tim Newsome <tim@sifive.com>
2019-01-07 12:46:34 -0800

    Use hwthread everywhere.

--- debug/targets/RISC-V/spike32-2-hwthread.py
+++ debug/targets/RISC-V/spike32-2-hwthread.py
@@ -5,7 +5,7 @@ import spike32  # pylint: disable=import-error
 
 class spike32_2(targets.Target):
     harts = [spike32.spike32_hart(), spike32.spike32_hart()]
-    openocd_config_path = "spike-2-hawt.cfg"
+    openocd_config_path = "spike-2-hwthread.cfg"
     timeout_sec = 30
     implements_custom_test = True
 
