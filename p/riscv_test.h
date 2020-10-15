1a3f249075292b9746897a69f3ba6eb2e7835141
Sandeep Rajendran <sandeep.rajendran@sifive.com>
2020-10-14 18:35:40 -0700

    unconditionally clear mie register

--- p/riscv_test.h
+++ p/riscv_test.h
@@ -115,11 +115,11 @@
 1:
 
 #define DELEGATE_NO_TRAPS                                               \
+  csrwi mie, 0;                                                         \
   la t0, 1f;                                                            \
   csrw mtvec, t0;                                                       \
   csrwi medeleg, 0;                                                     \
   csrwi mideleg, 0;                                                     \
-  csrwi mie, 0;                                                         \
   .align 2;                                                             \
 1:
 
