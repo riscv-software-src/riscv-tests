67cb46859f51e71d4fc17df22673d7410056fbb7
Tim Newsome <tim@sifive.com>
2020-12-14 12:40:54 -0800

    Add tests for memory sampling feature. (#300)

--- debug/targets/RISC-V/spike32-2-hwthread.py
+++ debug/targets/RISC-V/spike32-2-hwthread.py
@@ -9,6 +9,7 @@ class spike32_2(targets.Target):
     openocd_config_path = "spike-2-hwthread.cfg"
     timeout_sec = 5
     implements_custom_test = True
+    support_memory_sampling = False # not supported without sba
 
     def create(self):
         return testlib.Spike(self, isa="RV32IMAV", support_hasel=True,
--- debug/targets/RISC-V/spike64-2-hwthread.py
+++ debug/targets/RISC-V/spike64-2-hwthread.py
@@ -9,6 +9,7 @@ class spike64_2(targets.Target):
     openocd_config_path = "spike-2-hwthread.cfg"
     timeout_sec = 5
     implements_custom_test = True
+    support_memory_sampling = False # Needs SBA
 
     def create(self):
         return testlib.Spike(self)
