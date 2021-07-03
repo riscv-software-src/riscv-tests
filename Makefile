78907270e78d18979dd32f3a4c1f91e9f0d04e9e
181250012-Chen Lu <181250012@smail.nju.edu.cn>
2021-07-03 12:27:09 +0800

    adapt rv32ui and rv64ui to am

---
+++ Makefile
@@ -0,0 +1,33 @@
+.PHONY: all run clean $(ALL)
+
+SUPPORTED_ISA = riscv64 riscv32
+
+RISCV = $(word 1, $(subst -, ,$(ARCH)))
+ifeq ($(filter $(SUPPORTED_ISA), $(RISCV)), )
+  $(error Expected $$ISA in {$(SUPPORTED_ISA)}, Got "$(RISCV)")
+endif
+
+ifeq ($(RISCV), riscv64)
+  RV_DIR = isa/rv64ui
+else
+  RV_DIR = isa/rv32ui
+endif
+
+ALL = $(basename $(notdir $(shell find $(RV_DIR)/. -name "*.S")))
+
+all: $(addprefix Makefile-, $(ALL))
+	@echo "" $(ALL)
+
+$(ALL): %: Makefile-%
+
+Makefile-%: $(RV_DIR)/%.S
+	@/bin/echo -e "NAME = $*\nSRCS = $<\nLIBS += klib\nINC_PATH += $(shell pwd)/env/p $(shell pwd)/isa/macros/scalar\ninclude $${AM_HOME}/Makefile" > $@
+	-@make -s -f $@ ARCH=$(ARCH) $(MAKECMDGOALS)
+	-@rm -f Makefile-$*
+
+# cancel rules included by $(AM_HOME)/Makefile.check
+image: ;
+default $(MAKECMDGOALS): all ;
+
+clean:
+	rm -rf Makefile-* build/
--- env/p/riscv_test.h
+++ env/p/riscv_test.h
@@ -4,7 +4,6 @@
 #define _ENV_PHYSICAL_SINGLE_CORE_H
 
 #include "../encoding.h"
-
 //-----------------------------------------------------------------------
 // Begin Macro
 //-----------------------------------------------------------------------
@@ -162,65 +161,7 @@
         .weak stvec_handler;                                            \
         .weak mtvec_handler;                                            \
         .globl _start;                                                  \
-_start:                                                                 \
-        /* reset vector */                                              \
-        j reset_vector;                                                 \
-        .align 2;                                                       \
-trap_vector:                                                            \
-        /* test whether the test came from pass/fail */                 \
-        csrr t5, mcause;                                                \
-        li t6, CAUSE_USER_ECALL;                                        \
-        beq t5, t6, write_tohost;                                       \
-        li t6, CAUSE_SUPERVISOR_ECALL;                                  \
-        beq t5, t6, write_tohost;                                       \
-        li t6, CAUSE_MACHINE_ECALL;                                     \
-        beq t5, t6, write_tohost;                                       \
-        /* if an mtvec_handler is defined, jump to it */                \
-        la t5, mtvec_handler;                                           \
-        beqz t5, 1f;                                                    \
-        jr t5;                                                          \
-        /* was it an interrupt or an exception? */                      \
-  1:    csrr t5, mcause;                                                \
-        bgez t5, handle_exception;                                      \
-        INTERRUPT_HANDLER;                                              \
-handle_exception:                                                       \
-        /* we don't know how to handle whatever the exception was */    \
-  other_exception:                                                      \
-        /* some unhandlable exception occurred */                       \
-  1:    ori TESTNUM, TESTNUM, 1337;                                     \
-  write_tohost:                                                         \
-        sw TESTNUM, tohost, t5;                                         \
-        j write_tohost;                                                 \
-reset_vector:                                                           \
-        INIT_XREG;                                                      \
-        RISCV_MULTICORE_DISABLE;                                        \
-        INIT_SATP;                                                      \
-        INIT_PMP;                                                       \
-        DELEGATE_NO_TRAPS;                                              \
-        li TESTNUM, 0;                                                  \
-        la t0, trap_vector;                                             \
-        csrw mtvec, t0;                                                 \
-        CHECK_XLEN;                                                     \
-        /* if an stvec_handler is defined, delegate exceptions to it */ \
-        la t0, stvec_handler;                                           \
-        beqz t0, 1f;                                                    \
-        csrw stvec, t0;                                                 \
-        li t0, (1 << CAUSE_LOAD_PAGE_FAULT) |                           \
-               (1 << CAUSE_STORE_PAGE_FAULT) |                          \
-               (1 << CAUSE_FETCH_PAGE_FAULT) |                          \
-               (1 << CAUSE_MISALIGNED_FETCH) |                          \
-               (1 << CAUSE_USER_ECALL) |                                \
-               (1 << CAUSE_BREAKPOINT);                                 \
-        csrw medeleg, t0;                                               \
-1:      csrwi mstatus, 0;                                               \
-        init;                                                           \
-        EXTRA_INIT;                                                     \
-        EXTRA_INIT_TIMER;                                               \
-        la t0, 1f;                                                      \
-        csrw mepc, t0;                                                  \
-        csrr a0, mhartid;                                               \
-        mret;                                                           \
-1:
+_start:
 
 //-----------------------------------------------------------------------
 // End Macro
@@ -234,21 +175,25 @@ reset_vector:                                                           \
 //-----------------------------------------------------------------------
 
 #define RVTEST_PASS                                                     \
-        fence;                                                          \
-        li TESTNUM, 1;                                                  \
-        li a7, 93;                                                      \
-        li a0, 0;                                                       \
-        ecall
+          li a0, 0;                                                   \
+          j halt
+        // fence;                                                          \
+        // li TESTNUM, 1;                                                  \
+        // li a7, 93;                                                      \
+        // li a0, 0;                                                       \
+        // ecall
 
 #define TESTNUM gp
 #define RVTEST_FAIL                                                     \
-        fence;                                                          \
-1:      beqz TESTNUM, 1b;                                               \
-        sll TESTNUM, TESTNUM, 1;                                        \
-        or TESTNUM, TESTNUM, 1;                                         \
-        li a7, 93;                                                      \
-        addi a0, TESTNUM, 0;                                            \
-        ecall
+          li a0, 1;                                                      \
+          j halt
+//         fence;                                                          \
+// 1:      beqz TESTNUM, 1b;                                               \
+//         sll TESTNUM, TESTNUM, 1;                                        \
+//         or TESTNUM, TESTNUM, 1;                                         \
+//         li a7, 93;                                                      \
+//         addi a0, TESTNUM, 0;                                            \
+//         ecall
 
 //-----------------------------------------------------------------------
 // Data Section Macro
