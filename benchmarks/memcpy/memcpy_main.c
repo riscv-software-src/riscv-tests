044a6a06f2a35d22c4d13c9a0336125af87b62ca
Jerry Zhao <jerryz123@berkeley.edu>
2023-05-22 14:52:40 -0700

    Add scalar single-threaded/mt memcpy

---
+++ benchmarks/memcpy/memcpy_main.c
@@ -0,0 +1,39 @@
+// See LICENSE for license details.
+
+//**************************************************************************
+// Memcpy benchmark
+//--------------------------------------------------------------------------
+//
+// This benchmark tests the memcpy implementation in syscalls.c.
+// The input data (and reference data) should be generated using
+// the memcpy_gendata.pl perl script and dumped to a file named
+// dataset1.h.
+
+#include <string.h>
+#include "util.h"
+
+//--------------------------------------------------------------------------
+// Input/Reference Data
+
+#include "dataset1.h"
+
+//--------------------------------------------------------------------------
+// Main
+
+int main( int argc, char* argv[] )
+{
+  int results_data[DATA_SIZE];
+
+#if PREALLOCATE
+  // If needed we preallocate everything in the caches
+  memcpy(results_data, input_data, sizeof(int) * DATA_SIZE);
+#endif
+
+  // Do the riscv-linux memcpy
+  setStats(1);
+  memcpy(results_data, input_data, sizeof(int) * DATA_SIZE); //, DATA_SIZE * sizeof(int));
+  setStats(0);
+
+  // Check the results
+  return verify( DATA_SIZE, results_data, input_data );
+}
---
+++ benchmarks/mt-memcpy/mt-memcpy.c
@@ -0,0 +1,68 @@
+// See LICENSE for license details.
+
+//**************************************************************************
+// Memcpy benchmark
+//--------------------------------------------------------------------------
+// Author  : Jerry Zhao
+// TA      :
+// Student :
+//
+// This benchmark tests the memcpy implementation in syscalls.c.
+// The input data (and reference data) should be generated using
+// the memcpy_gendata.pl perl script and dumped to a file named
+// dataset1.h.
+
+
+//--------------------------------------------------------------------------
+// Includes
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+//--------------------------------------------------------------------------
+// Input/Reference Data
+
+#include "dataset1.h"
+
+
+//--------------------------------------------------------------------------
+// Basic Utilities and Multi-thread Support
+
+#include "util.h"
+
+
+
+//--------------------------------------------------------------------------
+// Main
+//
+// all threads start executing thread_entry(). Use their "coreid" to
+// differentiate between threads (each thread is running on a separate core).
+
+void thread_entry(int cid, int nc)
+{
+   // static allocates data in the binary, which is visible to both threads
+   static int results_data[DATA_SIZE];
+
+   size_t block = (DATA_SIZE / nc) + 1;
+   size_t n = (nc == cid + 1) ? DATA_SIZE - cid * block : block;
+
+
+   // First do out-of-place vvadd
+#if PREALLOCATE
+   barrier(nc);
+   memcpy(results_data + block * cid, input_data + block * cid, sizeof(int) * n);
+#endif
+
+   barrier(nc);
+   stats(memcpy(results_data + block * cid, input_data + block * cid, sizeof(int) * n); barrier(nc), DATA_SIZE);
+  /* barrier(nc); */
+   /* if(cid == 0) { */
+   /*   int res = verify(DATA_SIZE, results_data, input_data); */
+   /*   if(res) exit(res); */
+   /* } */
+
+   barrier(nc);
+   exit(0);
+}
