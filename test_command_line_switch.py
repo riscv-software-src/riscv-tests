5b864457f5c937d83a00e2ac0886fc20903c5a6f
William McSpaddden <bill@riscv.org>
2024-04-29 20:42:01 -0500

    added sail-specific,  python fragments for use by run_test.py

---
+++ test_command_line_switch.py
@@ -0,0 +1,9 @@
+
+sim_test_command_line_switch_dict = {
+    #  test name              sim command line switch
+    "rv32ui-v-ma_data"      : "--enable-misaligned",
+    "rv32ui-p-ma_data"      : "--enable-misaligned",
+    "rv64ui-v-ma_data"      : "--enable-misaligned",
+    "rv64ui-p-ma_data"      : "--enable-misaligned",
+    }
+
---
+++ test_ignore_list.py
@@ -0,0 +1,7 @@
+
+# Used by run_tests.py.  This is a list of tests to be ignored
+# when running a set of tests.
+
+ignore_test_tuple = [
+    "rv64ssvnapot-p-napot",
+    ]
